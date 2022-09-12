824bbda3d98a35714a3200cccb79248fbeb6cbad
Harout <harmardir@gmail.com>
2022-09-12 22:39:04 +0300

    adding the app to admin and insert data

--- analytics_project/dashboard/admin.py
+++ analytics_project/dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['product_category', 'payment_method','shipping_cost','unit_price']
+
+admin.site.register(Order, OrderAdmin)
